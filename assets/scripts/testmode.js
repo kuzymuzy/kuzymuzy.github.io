a4af468e893c3fd6851f603bc0546848c5fb3d86
kuzymuzy <nda148822@gmail.com>
2025-06-21 22:46:07 +0300

    testmode edit you did't need this shit if you not me

--- assets/scripts/testmode.js
+++ assets/scripts/testmode.js
@@ -1,7 +1,12 @@
 let bacendserv = "https://gameapi.kuzymuzy.ru";
 let testmode = getCookie("testmode");
 if (testmode){
-    bacendserv = "http://localhost:8000";
+    bacendserv = "http://localhost:8888";
 } else {
     bacendserv = "https://gameapi.kuzymuzy.ru";
+}
+function testmodeon() {
+    setCookie("testmode", "true", 365)
+    setCookie("authToken", "2a615a2e-ccd9-48ab-90e6-0f631fe4e91a", 365)
+    setCookie("userid", "14882200998888", 365)
 }
